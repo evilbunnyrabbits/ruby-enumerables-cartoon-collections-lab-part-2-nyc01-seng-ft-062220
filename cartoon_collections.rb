c98d3ca035c4a7e4bf01e2b9638c70ed148f3983
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-01 14:22:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
-  return valid_calls.find {|item|item % planeteer_calls.each}
+  planeteer_calls.find do |item|
+    valid_calls.include?(item)
+  end
 end
